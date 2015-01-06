fab61d59922c53f648fa8228aa29b245aa544ce5
Bogdan Tirca <bogdantirca@Bogdans-MacBook-Pro-2.local>
2015-01-07 00:57:43 +0200

    Bot to cheat at 10fastfinger

---
+++ 10fastfinger.js
@@ -0,0 +1,20 @@
+var startPlay = function() {
+    var kids = $("#row1").children();
+    var index = 0;
+    var iterate = function() {
+      if (index >= kids.length) {
+        clearInterval(interval);
+        return;
+      }
+      var word = $(kids[index]).html();
+      $("#inputfield").val(word + ' ');
+
+      var e = $.Event('keyup');
+      e.which= 32; // space
+      $('#inputfield').trigger(e);
+
+      index += 1;
+    };
+    var interval = setInterval(iterate, 300);
+  };
+  setTimeout(startPlay, 500);
